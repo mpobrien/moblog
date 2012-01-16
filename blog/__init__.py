1e184639ba3e770302c940cc1f8825a37b5744c1
Mike O'Brien <mikeo@10gen.com>
2012-01-16 17:34:56 -0500

    use mongohq urls

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 #app.debug = True
 app.secret_key = '"\x97\xaa0\x87\xf3T\x95W\xeb\x1eS\xc2\x00X\x1c\xa7\'\xda\xc5\x82\xc0|\xec\xa0"'
 app.admins = ['mpobrien']
+
+env_var = os.getenv('ENV')
+if env_var is not None and env_var == 'REMOTE':
+    db = Connection(os.getenv('MONGOHQ_URL'))
+else:
+    db = Connection()['blog']
+
+
 
 #MONGODB_HOST = 'localhost'
 #MONGODB_PORT = 27017
