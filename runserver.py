6e777fbc33dfd925f6f1e93b5010041c5a870ef2
Mike O'Brien <mikeo@10gen.com>
2012-01-16 17:20:44 -0500

    cedar stuff

--- runserver.py
+++ runserver.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from blog import app
+import os
 app.debug = True
 if __name__ == '__main__':
-  app.run(port=80,host='0.0.0.0')
+  port = int(os.environ.get("PORT", 5001))
+  app.run(port=port,host='0.0.0.0')
